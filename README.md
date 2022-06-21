278dac34d6163d64ac16429b9cdaee7c1b67db5c
Juan Carlos Olivier <juanc.olivierj@gmail.com>
2022-06-20 23:54:30 -0500

    new branch

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ReactTS
+https://javascript.plainenglish.io/webpack-in-2021-typescript-jest-sass-eslint-7b4640842e27
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+import './styles.scss';
+import { Component } from './components/components';
+
+const component = new Component();
+component.hello();
