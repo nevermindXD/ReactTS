e0fdf0291259eed08b1c24e0fa89889e61f24a6c
Juan Carlos Olivier <juanc.olivierj@gmail.com>
2022-06-22 00:13:37 -0500

    back to normal route

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,34 @@
-import React,{ Suspense } from 'react';
+// import React,{ Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// import Home from "./components/home";
-// import Settings from "./components/settings";
-// import Topics from "./components/topics";
-// import NotFound from "./components/404";
+import Home from "./components/home";
+import Settings from "./components/settings";
+import Topics from "./components/topics";
+import NotFound from "./components/404";
 
-const NotFound = React.lazy(() => import('./components/404'));
-const Home = React.lazy(() => import('./components/home'));
-const Settings = React.lazy(() => import('./components/settings'));
-const Topics = React.lazy(() => import('./components/topics'));
+// const NotFound = React.lazy(() => import('./components/404'));
+// const Home = React.lazy(() => import('./components/home'));
+// const Settings = React.lazy(() => import('./components/settings'));
+// const Topics = React.lazy(() => import('./components/topics'));
 
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path='*' element={<NotFound/>} />
-                    <Route path="/" element={<App/>}/>
-                    <Route path="/home" element={<Home/>}/>
-                    <Route path="/settings" element={<Settings/>}/>
-                    <Route path="/topics" element={<Topics/>}/>
-                </Routes>
-            </Suspense>
+            {/* <Suspense fallback={<div>Loading...</div>}> */}
+            <Routes>
+                <Route path='*' element={<NotFound/>} />
+                <Route path="/" element={<App/>}/>
+                <Route path="/home" element={<Home/>}/>
+                <Route path="/settings" element={<Settings/>}/>
+                <Route path="/topics" element={<Topics/>}/>
+            </Routes>
+            {/* </Suspense> */}
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root'),
 );
+
