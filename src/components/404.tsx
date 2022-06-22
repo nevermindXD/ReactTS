6afd42cc36faa6522efe63feef2113acca336d13
Juan Carlos Olivier <juanc.olivierj@gmail.com>
2022-06-22 00:05:59 -0500

    add react lazy

---
+++ src/components/404.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const NotFound = () => {
+    return <p>Not found</p>;
+}
+export default NotFound;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React,{ Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "./components/home";
-import Settings from "./components/settings";
-import Topics from "./components/topics";
+// import Home from "./components/home";
+// import Settings from "./components/settings";
+// import Topics from "./components/topics";
+// import NotFound from "./components/404";
+
+const NotFound = React.lazy(() => import('./components/404'));
+const Home = React.lazy(() => import('./components/home'));
+const Settings = React.lazy(() => import('./components/settings'));
+const Topics = React.lazy(() => import('./components/topics'));
+
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App/>}/>
-                <Route path="/home" element={<Home/>}/>
-                <Route path="/settings" element={<Settings/>}/>
-                <Route path="/topics" element={<Topics/>}/>
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='*' element={<NotFound/>} />
+                    <Route path="/" element={<App/>}/>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/settings" element={<Settings/>}/>
+                    <Route path="/topics" element={<Topics/>}/>
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root'),
